14f9796d307742730851ff007cdab69fa2b8ea99
saleh <ghalayinisaleh9@gmail.com>
2024-11-05 20:46:07 +0200

    created a priorityQueue and added all the nodes that consist of the bytes aka characters and their frequencies in the generateTree method

--- src/ByteFrequencyEncoder.java
+++ src/ByteFrequencyEncoder.java
@@ -1,4 +1,6 @@
 import java.util.LinkedHashMap;
+import java.util.PriorityQueue;
+
 
 public class ByteFrequencyEncoder {
 
@@ -9,7 +11,12 @@ public class ByteFrequencyEncoder {
     
     public void generateTree(LinkedHashMap<Integer, Integer> sorted_frequency)
     {
-        
+        PriorityQueue<Node> node_queue = new PriorityQueue<>();
+         
+        sorted_frequency.forEach((key, value) -> {
+            Node new_node = new Node(key, value); //creating new nodes for each linkedhashmap key/value
+            node_queue.add(new_node);   //adding it to the priorityQueue
+        });
     }
 
 }
--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,15 @@
 public class Node {
-    
+    int byte_value;
+    int frequency;
+    Node left;
+    Node right;
+
+    public Node(int byte_value, int frequency)
+    {
+        this.byte_value = byte_value;
+        this.frequency = frequency;
+        left = null;
+        right = null;
+    }
+
 }
